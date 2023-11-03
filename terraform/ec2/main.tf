23373040380704d9b6c0f126cc45bffabc679ff0
zenfolio-hiennguyen <hnguyen@zenfolio.com>
2023-11-04 00:05:10 +0700

    Use Terraform to provision VPC, EC2, ECR and EKS on AWS

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -1,10 +1,24 @@
-module "bastion-host" {
-  source = "../modules/ec2"
-  name                          = "Bastion-host"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
 
-  ec2_instances                 = local.bastion_hosts
-  generate_ssh_key_pair         = true
-  owner                         = var.owner
-  project                       = var.project
-  environment                   = var.environment
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-189f843e"]
+  subnet_id= "subnet-0688d3344bb77143d"
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
 }
